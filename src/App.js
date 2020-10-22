f3b7778d7da861811a59eca81910cebde8d907b8
jaballogian <jaballogianrma77@gmail.com>
2020-10-22 16:19:24 +0700

    added html, css, and javascript code and converted to react code convention in Custom Theme example

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import React from 'react'
 import CategoryWidget from './Components/Category Widget/CategoryWidget'
 import BasicLayout from './Examples/Basic Layout/BasicLayout'
 import SimpleLayout from './Examples/Simple Layout/SimpleLayout'
+import CustomTheme from './Examples/Custom Theme/CustomTheme'
 
 function App() {
   return (
-    <SimpleLayout></SimpleLayout>
+    <CustomTheme></CustomTheme>
   )
 }
 
