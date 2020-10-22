8de43c1b88b3e3d92304b2f4266952c6470247e0
jaballogian <jaballogianrma77@gmail.com>
2020-10-22 10:41:02 +0700

    installed mapbox gl dependency to the project, added mapbox gl css to index html file, added mapbox gl map to Basic Layout file, and fixed styling of Map and Map Panel texts in Basic Layout

--- src/App.js
+++ src/App.js
@@ -3,12 +3,8 @@ import CategoryWidget from './Components/Category Widget/CategoryWidget'
 import BasicLayout from './Examples/Basic Layout/BasicLayout'
 
 function App() {
-
   return (
-    <div>
-      {/* <CategoryWidget/> */}
-      <BasicLayout/>
-    </div>
+    <BasicLayout></BasicLayout>
   )
 }
 
--- src/Examples/Basic Layout/BasicLayout.js
+++ src/Examples/Basic Layout/BasicLayout.js
@@ -1,62 +1,89 @@
-import React from 'react'
-import '@carto/airship-style/dist/airship.css';
-import { defineCustomElements } from '@carto/airship-components/dist/loader';
+import React, {useEffect, useRef} from 'react'
+import '@carto/airship-style/dist/airship.css'
+import { defineCustomElements } from '@carto/airship-components/dist/loader'
+import mapboxgl from 'mapbox-gl'
 
 defineCustomElements(window);
 
 function BasicLayout(){
+
+    const mapAreaText = {
+        position: "absolute",
+        zIndex: "10px",
+        top: "24px",
+        left: "8px",
+        padding: "8px",
+        backgroundColor: "#FFF"
+    };
+
+    const panelText = {
+        marginRight: "8px",
+        padding: "8px",
+        backgroundColor: "#FFF"
+    }
+
+    useEffect(() => {
+        const map = new mapboxgl.Map({
+            container: 'map',
+            style: 'https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json',
+            center: [0, 30],
+            zoom: 2,
+            scrollZoom: false,
+            dragRotate: false,
+            touchZoomRotate: false
+        });
+      }, [])
+
     return(
-        <div>
-            <body class="as-app-body as-app">
+        <body className="as-app-body as-app">
 
-                <header className="as-toolbar">
-                    <div className="as-toolbar__item as-title">TOOLBAR</div>
-                </header>
-                <nav className="as-tabs"></nav>
+            <header className="as-toolbar">
+                <div className="as-toolbar__item as-title">TOOLBAR</div>
+            </header>
+            <nav className="as-tabs"></nav>
 
-                <div className="as-content">
+            <div className="as-content">
 
-                    <aside className="as-sidebar as-sidebar--left">
-                    <div className="as-container">
-                        <div className="as-box as-title">
-                        LEFT SIDEBAR
-                        </div>
+                <aside className="as-sidebar as-sidebar--left">
+                <div className="as-container">
+                    <div className="as-box as-title">
+                    LEFT SIDEBAR
                     </div>
-                    </aside>
+                </div>
+                </aside>
 
-                    <main className="as-main">
-                    <div className="as-map-area">
-                        <div className="map-area-text as-title">MAP</div>
-                        <div id="map"></div>
+                <main className="as-main">
+                <div className="as-map-area">
+                    <div id="map"></div>
+                    <div className="as-title" style={mapAreaText}>MAP</div>
 
-                        <div className="as-map-panels">
-                        <div className="as-panel as-panel--top as-panel--right">
-                            <div className="as-title panel-text">MAP PANELS</div>
-                            <div className="as-panel__element as-p--32 as-bg--warning"></div>
-                            <div className="as-panel__element as-p--32 as-bg--success"></div>
-                        </div>
-                        </div>
-
+                    <div className="as-map-panels">
+                    <div className="as-panel as-panel--top as-panel--right">
+                        <div className="as-title" style={panelText}>MAP PANELS</div>
+                        <div className="as-panel__element as-p--32 as-bg--warning"></div>
+                        <div className="as-panel__element as-p--32 as-bg--success"></div>
+                    </div>
                     </div>
 
-                    <footer className="as-map-footer as-bg--complementary" style={{height: 100}}>
-                        <div className="as-box as-title as-color--type-04">
-                        MAP FOOTER
-                        </div>
-                    </footer>
-                    </main>
-
-                    <aside className="as-sidebar as-sidebar--right">
-                    <div className="as-container">
-                        <div className="as-box as-title">
-                        RIGHT SIDEBAR
-                        </div>
-                    </div>
-                    </aside>
                 </div>
 
-            </body>
-        </div>
+                <footer className="as-map-footer as-bg--complementary" style={{height: 100}}>
+                    <div className="as-box as-title as-color--type-04">
+                    MAP FOOTER
+                    </div>
+                </footer>
+                </main>
+
+                <aside className="as-sidebar as-sidebar--right">
+                <div className="as-container">
+                    <div className="as-box as-title">
+                    RIGHT SIDEBAR
+                    </div>
+                </div>
+                </aside>
+            </div>
+
+        </body>
     )
 }
 
